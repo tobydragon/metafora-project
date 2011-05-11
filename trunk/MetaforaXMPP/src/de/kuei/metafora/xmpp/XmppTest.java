8315e9aaeeeba2409934b1c0b9d7a14c88751414
irgang.la@googlemail.com <irgang.la@googlemail.com@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2011-05-11 17:14:51 +0000

    support for parallel connections added

--- trunk/MetaforaXMPP/src/de/kuei/metafora/xmpp/XmppTest.java
+++ trunk/MetaforaXMPP/src/de/kuei/metafora/xmpp/XmppTest.java
@@ -12,10 +12,23 @@ public class XmppTest {
 		};
 		
 		try {
-			XMPPBridge bridge = XMPPBridge.getInstance();
+			XMPPBridge bridge = XMPPBridge.getTestConnection();
 			bridge.connect(true);
 			bridge.registerListener(listener);
 			bridge.sendMessage("XmppTest is online and waiting for messages!");
+			
+			XMPPBridge.createConnection("thomas", "thomas", "didPfT", "logger@conference.metafora.ku-eichstaett.de", "TestThomas", "Test");
+			XMPPBridge bridge2 = XMPPBridge.getConnection("thomas");
+			bridge2.connect(false);
+			bridge2.registerListener(listener);
+			bridge2.sendMessage("XmppTest 2 is online and waiting for messages!");
+			
+			XMPPBridge.createConnection("newuser", "ANewUser", "didPfNU", "logger@conference.metafora.ku-eichstaett.de", "new User", "Test");
+			XMPPBridge bridge3 = XMPPBridge.getConnection("newuser");
+			bridge3.connect(true);
+			bridge3.registerListener(listener);
+			bridge3.sendMessage("XmppTest 3 is online and waiting for messages!");
+			
 			while(true){
 				
 			}
