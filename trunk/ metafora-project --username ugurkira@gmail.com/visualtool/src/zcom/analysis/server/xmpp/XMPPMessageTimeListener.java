6786be026bdff62aeba4f733072003b24c4234e6
ugurkira <ugurkira@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2012-03-09 10:56:03 +0000

    Removing unused classes

--- trunk/ metafora-project --username ugurkira@gmail.com/visualtool/src/zcom/analysis/server/xmpp/XMPPMessageTimeListener.java
+++
@@ -1,9 +0,0 @@
-package zcom.analysis.server.xmpp;
-
-import java.util.Date;
-
-public interface XMPPMessageTimeListener {
-
-	public void newMessage(String user, String message, String chat, Date time);
-	
-}
