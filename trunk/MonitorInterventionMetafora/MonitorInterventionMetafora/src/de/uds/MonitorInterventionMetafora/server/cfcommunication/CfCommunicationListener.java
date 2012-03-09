16380e8c70fb57b3c05fe26ff07bdfdd5b66be23
tdragon1999@gmail.com <tdragon1999@gmail.com@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2012-03-09 12:49:42 +0000

    remove to rename

--- trunk/MonitorInterventionMetafora/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/cfcommunication/CfCommunicationListener.java
+++
@@ -1,9 +0,0 @@
-package de.uds.MonitorInterventionMetafora.server.cfcommunication;
-
-import de.uds.MonitorInterventionMetafora.shared.commonformat.CfAction;
-
-public interface CfCommunicationListener {
-	
-	public void processCfAction(String user, CfAction action);
-
-}
