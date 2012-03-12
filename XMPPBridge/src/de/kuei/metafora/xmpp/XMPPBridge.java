2e1482160ca961bc20d8529c5adf872a0ae436ff
irgang.la <irgang.la@gmail.com>
2012-03-12 16:26:10 +0000

    New Version of the XMPP Bridge with auto reconnect if connection is lost by a watchdog and splitted account and chat management. The new mainclass is XMPPBridgeCurrent and XMPPBridge provides a compatibility layer.
    PLEASE USE THE JAR-FILE INSTEAD!!
    
    New package name: de.kuei.metafora.xmpp

---
+++ XMPPBridge/src/de/kuei/metafora/xmpp/XMPPBridge.java
@@ -0,0 +1,117 @@
+package de.kuei.metafora.xmpp;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class XMPPBridge {
+
+	private static Map<String, XMPPBridge> nameToBridge = Collections
+			.synchronizedMap(new HashMap<String, XMPPBridge>());
+	private static Map<String, XMPPBridgeCurrent> userToCurrentBridge = Collections
+			.synchronizedMap(new HashMap<String, XMPPBridgeCurrent>());
+
+	public static XMPPBridge getConnection(String name) {
+		return nameToBridge.get(name);
+	}
+
+	public static XMPPBridge getTestConnection() {
+		if(!nameToBridge.containsKey("test")){
+			XMPPBridge.createConnection("test", "testuser", "didPfT",
+					"logger@conference.metafora.ku-eichstaett.de", "Testuser",
+					"Testdevice");
+		}
+		return getConnection("test");
+		
+	}
+
+	public static synchronized void createConnection(String connectionname, String user,
+			String password, String chatroom, String alias, String device){
+		XMPPBridgeCurrent bridgeCurrent = null;
+		
+		if(!userToCurrentBridge.containsKey(user)){
+			XMPPBridgeCurrent.setUser(user, password);
+			XMPPBridgeCurrent.setAlias(alias);
+			XMPPBridgeCurrent.setDevice(device);
+			bridgeCurrent = new XMPPBridgeCurrent();
+			
+			bridgeCurrent.login(true);
+			
+			userToCurrentBridge.put(user, bridgeCurrent);
+		}else{
+			bridgeCurrent = userToCurrentBridge.get(user);
+		}
+		
+		XMPPBridge bridge = new XMPPBridge(bridgeCurrent, chatroom);
+		nameToBridge.put(connectionname, bridge);
+	}
+
+	private XMPPBridgeCurrent bridge;
+	private String chat;
+
+	private XMPPBridge(XMPPBridgeCurrent bridge, String chat) {
+		this.bridge = bridge;
+		this.chat = chat;
+		
+		this.bridge.connectToChat(chat);
+	}
+
+	public void init() {
+		// ignore
+	}
+
+	public void registerListener(XMPPMessageListener listener) {
+		this.bridge.getDistributor().addListener(listener);
+	}
+
+	public void registerTimeListener(XMPPMessageTimeListener listener) {
+		this.bridge.getDistributor().addTimeListener(listener);
+	}
+
+	public void removeListener(XMPPMessageListener listener) {
+		this.bridge.getDistributor().removeListener(listener);
+	}
+
+	public void removeTimeListener(XMPPMessageTimeListener listener) {
+		this.bridge.getDistributor().removeTimeListener(listener);
+	}
+
+	public void disconnect() {
+		this.bridge.shutdown();
+	}
+
+	public void connect(boolean register) {
+		bridge.login(register);
+	}
+
+	public void connect(String user, String password, boolean register) {
+		bridge.login(register);
+	}
+
+	public void connect(String user, String password, String device,
+			boolean register) {
+		bridge.login(register);
+	}
+
+	public void connectToChat() {
+		bridge.connectToChat(chat);
+	}
+
+	public void connectToChat(String chat) {
+		bridge.connectToChat(chat);
+	}
+
+	public void sendMessage(String message) {
+		bridge.sendMessageToMultiUserChat(chat, message, null);
+	}
+
+	public void sendMessageToUser(String message, String user) {
+		bridge.sendMessageToUser(message, user, null);
+	}
+
+	public Vector<String> getChatParticipants() {
+		return bridge.getChatParticipants(chat);
+	}
+
+}
---
+++ XMPPBridge/src/de/kuei/metafora/xmpp/XMPPBridgeCurrent.java
@@ -0,0 +1,399 @@
+package de.kuei.metafora.xmpp;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import org.jivesoftware.smack.AccountManager;
+import org.jivesoftware.smack.PacketCollector;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.RosterListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.Occupant;
+
+public class XMPPBridgeCurrent implements RosterListener{
+	
+	private static String SERVER = "metafora.ku-eichstaett.de";
+	private static String USER = "thomas";
+	private static String PASSWORD = "didPfT";
+	private static String ALIAS = null;
+	private static String DEVICE = "VipOWeb";
+	
+	public static final String available = "available";
+	public static final String donotdisturb = "do not disturb";
+	public static final String freetochat = "free to chat";
+	public static final String away = "away";
+	public static final String longtimeaway = "long time away";
+	public static final String unknown = "unknown";
+	public static final String notonline = "not online";
+		
+	private static XMPPBridgeCurrent instance = null;
+	
+	public static void setDevice(String device){
+		DEVICE = device;
+	}
+	
+	public static void setAlias(String alias){
+		ALIAS = alias;
+	}
+	
+	public static void setServer(String server){
+		SERVER = server;
+	}
+	
+	public static void setUser(String user, String password){
+		USER = user;
+		PASSWORD = password;
+	}
+	
+	public static XMPPBridgeCurrent getInstance() {
+		if(instance == null){
+			instance = new XMPPBridgeCurrent();
+		}
+		
+		return instance;
+	}
+
+	private XMPPConnection connection;
+	
+	private String server;
+	private String user;
+	private String password;
+	private String alias;
+	private String device;
+
+	private PacketCollector collector = null;
+	private XMPPMessageDistributor distributor = null;
+	private Roster roster;
+	private XMPPWatcher watcher;
+	
+	private Map<String, MultiUserChat> multiUserChats;
+	
+	private Map<String, String> userToName;
+	
+	public XMPPBridgeCurrent() {
+
+		this.server = XMPPBridgeCurrent.SERVER;
+		this.user = XMPPBridgeCurrent.USER;
+		this.password = XMPPBridgeCurrent.PASSWORD;
+		this.device = XMPPBridgeCurrent.DEVICE;
+		
+		if(XMPPBridgeCurrent.ALIAS != null){
+			this.alias = XMPPBridgeCurrent.ALIAS;
+		}else{
+			this.alias = XMPPBridgeCurrent.USER;
+		}
+		
+		connection = new XMPPConnection(this.server);
+		
+		multiUserChats = Collections.synchronizedMap(new HashMap<String, MultiUserChat>());
+		userToName = Collections.synchronizedMap(new HashMap<String, String>());
+	}
+
+	
+	public void login(boolean register){
+		try {
+			connection.connect();
+		} catch (XMPPException e1) {
+			System.err.println("XMPPBridge.login: connect failed!");
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			connection.login(this.user, this.password, this.device);
+		} catch (XMPPException e) {
+			if (register) {
+				AccountManager manager = connection.getAccountManager();
+				
+				try {
+					manager.createAccount(user, password);
+					
+					connection.disconnect();
+					connection = new XMPPConnection(this.server);
+					try{
+					connection.connect();
+					
+						try {
+							connection.login(this.user, this.password, this.device);
+						} catch (XMPPException ex) {
+							System.err.println("XMPPBridge.login: login after account creation failed!");
+							ex.printStackTrace();
+						}
+					} catch (XMPPException ex) {
+						System.err.println("XMPPBridge.login: connect after account creation failed!");
+						ex.printStackTrace();
+					}
+				} catch (XMPPException e1) {
+					System.err.println("XMPPBridge.login: create Account failed!");
+					e1.printStackTrace();
+				}
+			}else{
+				System.err.println("XMPPBridge.login: login failed!");
+				e.printStackTrace();
+			}
+		}
+		
+		if(connection.isConnected() && connection.isAuthenticated()){
+			collector = connection.createPacketCollector(null);
+			if(distributor == null){
+				distributor = new XMPPMessageDistributor();
+			}
+			distributor.start(collector);
+			roster = connection.getRoster();
+			for(RosterEntry entry : roster.getEntries()){
+				if(!userToName.containsKey(entry.getUser())){
+					userToName.put(entry.getUser(), entry.getName());
+					System.out.println("Roster: contact: Name:"+entry.getName()+" User: "+entry.getUser());
+				}else{
+					System.out.println("Roster: known contact: Name:"+entry.getName()+" User: "+entry.getUser());
+				}
+			}
+			
+			watcher = new XMPPWatcher(this);
+			new Thread(watcher).start();
+			
+		}else{
+			System.err.println("XMPPBridge.login: collector not initialized!");
+		}
+	}
+
+	public String getPresenceString(String user){
+		if(roster != null){
+			Presence presence = roster.getPresence(user);
+			if(presence != null){
+				if(presence.getType() == Presence.Type.available){
+					if(presence.getMode() == Presence.Mode.available){
+						return XMPPBridgeCurrent.available;
+					}else if(presence.getMode() == Presence.Mode.dnd){
+						return XMPPBridgeCurrent.donotdisturb;
+					}else if(presence.getMode() == Presence.Mode.away){
+						return XMPPBridgeCurrent.away;
+					}else if(presence.getMode() == Presence.Mode.chat){
+						return XMPPBridgeCurrent.freetochat;
+					}else if(presence.getMode() == Presence.Mode.xa){
+						return XMPPBridgeCurrent.longtimeaway;
+					}
+				}else if(presence.getType() == Presence.Type.unavailable){
+					return XMPPBridgeCurrent.notonline;
+				}
+			}
+		}else{
+			System.err.println("XMPPBridge.getPresenceString: roster not initialized!");
+		}
+		return XMPPBridgeCurrent.unknown;
+	}
+	
+	public void shutdown(){
+		watcher.stop();
+		disconnect();
+	}
+	
+	public void disconnect() {
+		if(distributor != null)
+			distributor.stop();
+		
+		connection.disconnect();
+	}
+
+	public void watcher(){
+		if(connection == null){
+			connection = new XMPPConnection(this.server);
+		}
+		
+		if(!connection.isConnected() || !connection.isAuthenticated()){
+			System.err.println("XMPPBridge.watcher: Bridge is not connected! Try to reconnect.");
+			
+			shutdown();
+			login(false);
+			
+			if(connection.isConnected() && connection.isAuthenticated()){
+				System.err.println("XMPPBridge.watcher: reconnect successful. Try to restart chats.");
+				restartMultiChats();
+			}else{
+				System.err.println("XMPPBridge.watcher: reconnect failed!");
+			}
+		}
+	}
+	
+	private void restartMultiChats(){
+		for(String chat : multiUserChats.keySet()){
+			System.err.println("XMPPBridge.restartMultiChats: restart chat "+chat);
+			MultiUserChat muc = multiUserChats.get(chat);
+			if(muc.isJoined())
+				muc.leave();
+			connectToChat(chat);
+		}
+	}
+	
+	public void connectToChat(String chat) {
+		if(connection.isConnected() && connection.isAuthenticated() && distributor != null){
+			MultiUserChat muc = null;
+			
+			if(multiUserChats.get(chat) == null){
+				muc = new MultiUserChat(connection, chat);
+				
+				multiUserChats.put(chat, muc);
+			}else{
+				muc = multiUserChats.get(chat);
+			}
+			
+			joinMultiUserChat(muc);
+			
+		}else{
+			System.err.println("XMPPBridge.connectToChat: user not logged in or not authorized!");
+		}
+	}
+
+	private void joinMultiUserChat(MultiUserChat muc){
+		try {
+			muc.join(alias);
+		} catch (XMPPException e) {
+			try {
+				muc.join(this.alias + System.currentTimeMillis());
+			} catch (XMPPException e1) {
+				System.err.println("XMPPBridge.joinMultiUserChat: multiuserchat could not be joined!");
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	public void sendMessageToMultiUserChat(String chat, String message, String subject) {
+		if(connection.isConnected() && connection.isAuthenticated()){
+			MultiUserChat muc = multiUserChats.get(chat);
+			
+			if(muc != null){
+				if(!muc.isJoined()){
+					joinMultiUserChat(muc);
+				}
+				try {
+					Message msg = muc.createMessage();
+					msg.setBody(message);
+					if(subject != null)
+						msg.setSubject(subject);
+					muc.sendMessage(msg);
+				} catch (XMPPException e) {
+					System.err.println("XMPPBridge.sendMessageToMultiUserChat: message send error!");
+					e.printStackTrace();
+				}
+			}else{
+				System.err.println("XMPPBridge.sendMessageToMultiUserChat: chat not found!");
+			}
+		}else{
+			System.err.println("XMPPBridge.sendMessageToMultiUserChat: user not logged in or not authorized!");
+		}
+	}
+
+	public void sendMessageToUser(String message, String user, String subject) {
+		if(connection.isConnected() && connection.isAuthenticated()){
+			Message msg = new Message();
+		    msg.setTo(user);
+		    if(subject != null){
+		    	msg.setSubject(subject);
+		    	msg.setType(Message.Type.headline);
+		    }
+		    msg.setBody(message);
+		    
+		    connection.sendPacket(msg);
+		}else{
+			System.err.println("XMPPBridge.sendMessageToMultiUserChat: user not logged in or not authorized!");
+		}
+	}
+	
+	public XMPPMessageDistributor getDistributor(){
+		return distributor;
+	}
+
+	@Override
+	public void entriesAdded(Collection<String> user) {
+		for(String uid : user){
+			RosterEntry entry = roster.getEntry(uid);
+			if(userToName.containsKey(entry.getUser())){
+				System.err.println("XMPPBridge.entiesAdded: known user added!");
+				userToName.remove(entry.getUser());
+			}
+			userToName.put(entry.getUser(), entry.getName());
+		}
+	}
+
+	@Override
+	public void entriesDeleted(Collection<String> user) {
+		for(String uid : user){
+			RosterEntry entry = roster.getEntry(uid);
+			userToName.remove(entry.getUser());
+		}
+	}
+
+	@Override
+	public void entriesUpdated(Collection<String> user) {
+		for(String uid : user){
+			RosterEntry entry = roster.getEntry(uid);
+			if(userToName.containsKey(entry.getUser())){
+				userToName.remove(entry.getUser());
+			}
+			userToName.put(entry.getUser(), entry.getName());
+		}
+	}
+
+	@Override
+	public void presenceChanged(Presence arg0) {
+		//ignore
+	}
+	
+	public Set<String> getKnownUsers(){
+		return userToName.keySet();
+	}
+
+	public String getUserName(String user){
+		if(userToName.containsKey(user))
+			return userToName.get(user);
+		return "unknown user";
+	}
+	
+	public Vector<String> getChatParticipants(String chat){
+		MultiUserChat muc = multiUserChats.get(chat);
+		
+		Vector<String> participants = new Vector<String>();
+		
+		if(muc != null){
+			try {
+				Collection<Occupant> parts = muc.getParticipants();
+				for(Occupant occ : parts){
+					participants.add(occ.getJid());
+				}
+				
+			} catch (XMPPException e) {
+				System.err.println("XMPPBridge.getChatParticipants: XMPP exception!");
+				e.printStackTrace();
+			}
+		}
+		
+		return participants;
+	}
+	
+	public String getRole(String chat, String user){
+		MultiUserChat muc = multiUserChats.get(chat);
+		if(muc != null){
+			Occupant occ = muc.getOccupant(user);
+			return occ.getRole();
+		}
+		return "unknown role";
+	}
+	
+	public String getNick(String chat, String user){
+		MultiUserChat muc = multiUserChats.get(chat);
+		if(muc != null){
+			Occupant occ = muc.getOccupant(user);
+			return occ.getNick();
+		}
+		return "unknown name";
+	}
+}
