aaf9a1d926aa617f1f131a57729a9fe324fdfaa5
ugurkira <ugurkira@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2012-03-22 01:59:12 +0000

    Adding dynamic filter at the top

--- MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/client/view/widgets/FilterListPanel.java
+++ MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/client/view/widgets/FilterListPanel.java
@@ -1,14 +1,14 @@
 package de.uds.MonitorInterventionMetafora.client.view.widgets;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.ScrollPanel;
+
 
 import de.uds.MonitorInterventionMetafora.client.communication.servercommunication.ActionMaintenance;
 import de.uds.MonitorInterventionMetafora.client.datamodels.FilterListGridModel;
@@ -51,12 +51,6 @@ public class FilterListPanel extends ContentPanel {
 	
 	
 	
-	public  void addFilter(String _property,String _value){
-		
-		ExtendedFilterItem efi=new ExtendedFilterItem(_property,_value);
-		this.add(efi);
-		_filterList.put(_property, _value);		
-	}
 	
 	
 
