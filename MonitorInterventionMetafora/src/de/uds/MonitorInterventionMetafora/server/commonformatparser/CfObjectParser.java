6e9d70f905398615c5bd549bd987116d15424906
toontalk <ToonTalk@gmail.com>
2012-04-25 15:26:26 +0000

    Merged two object elements in an action element.

--- MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/commonformatparser/CfObjectParser.java
+++ MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/commonformatparser/CfObjectParser.java
@@ -35,6 +35,10 @@ public class CfObjectParser {
 		
 		XmlFragment propertyFragment = xmlFragment.cloneChild(CommonFormatStrings.PROPERTIES_STRING);
 		Map<String, CfProperty> cfProperties = new HashMap<String, CfProperty>();
+		if (propertyFragment == null || 
+			propertyFragment.getChildren(CommonFormatStrings.PROPERTY_STRING) == null) {
+		    System.out.println("debug this");
+		}
 		for (XmlFragment cfPropertyElement : propertyFragment.getChildren(CommonFormatStrings.PROPERTY_STRING)){
 			CfProperty cfProperty = CfPropertyParser.fromXml(cfPropertyElement);
 			cfProperties.put(cfProperty.getName(), cfProperty);
