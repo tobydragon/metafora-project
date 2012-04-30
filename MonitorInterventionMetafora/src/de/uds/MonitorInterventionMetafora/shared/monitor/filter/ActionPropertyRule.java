3612af5ea5eeafa9e451a035ed4e2e94a78c210f
tdragon1999 <tdragon1999@gmail.com>
2012-04-30 18:47:03 +0000

    Added filters and groupings, fixed occured within functionality

--- MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/shared/monitor/filter/ActionPropertyRule.java
+++ MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/shared/monitor/filter/ActionPropertyRule.java
@@ -16,6 +16,7 @@ import de.uds.MonitorInterventionMetafora.shared.commonformat.CfAction;
 import de.uds.MonitorInterventionMetafora.shared.datamodels.attributes.ActionElementType;
 import de.uds.MonitorInterventionMetafora.shared.datamodels.attributes.OperationType;
 import de.uds.MonitorInterventionMetafora.shared.monitor.MonitorConstants;
+import de.uds.MonitorInterventionMetafora.shared.utils.GWTUtils;
 
 //a class representing a rule about an aspect of an action (to filter or group by).
 //This could be a standard attribute or a property of a CfAction...
@@ -36,9 +37,13 @@ public class ActionPropertyRule  implements Serializable{
 	}
 
 	public ActionPropertyRule(ActionElementType actionElementType, String propertyName) {
+		this(actionElementType, propertyName, propertyName);
+	}
+	
+	public ActionPropertyRule(ActionElementType actionElementType, String propertyName, String displayName) {
 		this.type = actionElementType;
 		this.propertyName = propertyName;
-		this.displayText = propertyName;
+		this.displayText = displayName;
 	}
   
   public ActionPropertyRule(String _entityName, String _value,ActionElementType _type,OperationType _operationtype) {
@@ -108,10 +113,13 @@ public class ActionPropertyRule  implements Serializable{
 
 	public String getActionValue(CfAction action) {
 		String actionValue = null;
+		try {
 		switch (getType()){
 			case ACTION_TYPE:
 				if (propertyName.equalsIgnoreCase("classification")){
 					actionValue=action.getCfActionType().getClassification();
+				} else if (propertyName.equalsIgnoreCase("type")){
+					actionValue=action.getCfActionType().getType();
 				}
 				break;
 			case USER:
@@ -125,34 +133,83 @@ public class ActionPropertyRule  implements Serializable{
 			case CONTENT:
 				if (propertyName.equalsIgnoreCase("TOOL")){
 					actionValue=action.getCfContent().getPropertyValue("TOOL");
+				} else if (propertyName.equalsIgnoreCase("INDICATOR_TYPE")){
+					actionValue=action.getCfContent().getPropertyValue("INDICATOR_TYPE");
+				} else if (propertyName.equalsIgnoreCase("CHALLENGE_NAME")){
+					actionValue=action.getCfContent().getPropertyValue("CHALLENGE_NAME");
+				} else if (propertyName.equalsIgnoreCase("GROUP_ID")){
+					actionValue=action.getCfContent().getPropertyValue("GROUP_ID");
+				} else if (propertyName.equalsIgnoreCase("description")){
+					actionValue=action.getCfContent().getDescription();
+				}
+				break;
+			case ACTION:
+				if (propertyName.equalsIgnoreCase("time")){
+					actionValue= Long.toString(action.getTime());
+				}
+				break;
+			case OBJECT:
+				//TODO: Deal with multiple objects
+				if (action.getCfObjects().size() > 0){
+					if (propertyName.equalsIgnoreCase("TOOL")){
+						actionValue=action.getCfObjects().get(0).getPropertyValue("TOOL");
+					} else if (propertyName.equalsIgnoreCase("INDICATOR_TYPE")){
+						actionValue=action.getCfObjects().get(0).getPropertyValue("INDICATOR_TYPE");
+					} else if (propertyName.equalsIgnoreCase("CHALLENGE_NAME")){
+						actionValue=action.getCfObjects().get(0).getPropertyValue("CHALLENGE_NAME");
+					} else if (propertyName.equalsIgnoreCase("GROUP_ID")){
+						actionValue=action.getCfObjects().get(0).getPropertyValue("GROUP_ID");
+					} else if (propertyName.equalsIgnoreCase("type")){
+						actionValue=action.getCfObjects().get(0).getType();
+					} else if (propertyName.equalsIgnoreCase("id")){
+						actionValue=action.getCfObjects().get(0).getId();
+					} 
 				}
 				break;
 			
+		}
+		}
+		catch(Exception e){
+			System.out.println("[ActionPropertyRule.getActionValue] No value found in action=\n" + "\nRule:" + this);
 		}
 		return actionValue;
 	}
 	
 	public boolean ruleIncludesAction(CfAction action){
-		if(valueToFilterBy != null){
-			String actionValue = getActionValue(action);
-			if (actionValue != null){
-				if (operationtype == OperationType.EQUALS){
-					return valueToFilterBy.equalsIgnoreCase(actionValue);
-				}
-				else if (operationtype == OperationType.CONTAINS){
-					return actionValue.toLowerCase().contains(valueToFilterBy.toLowerCase());
+		try {
+			if(valueToFilterBy != null){
+				String actionValue = getActionValue(action);
+				if (actionValue != null){
+					if (operationtype == OperationType.EQUALS){
+						return valueToFilterBy.equalsIgnoreCase(actionValue);
+					}
+					else if (operationtype == OperationType.CONTAINS){
+						return actionValue.toLowerCase().contains(valueToFilterBy.toLowerCase());
+					}
+					else if (operationtype == OperationType.OCCUREDWITHIN){
+						long timeStamp = Long.valueOf(actionValue);
+						if(GWTUtils.getDifferenceInMinutes(timeStamp) <= Integer.valueOf(valueToFilterBy)){
+							return true;
+						}
+						else {
+							return false;
+						}
+					}
+					else {
+						System.err.println("ERROR:\t\t[ActionProperty.ruleIncludesAction] Unrecognized OperationType=" + operationtype);
+					}
 				}
 				else {
-					System.err.println("ERROR:\t\t[ActionProperty.ruleIncludesAction] Unrecognized OperationType=" + operationtype);
+					return MonitorConstants.BLANK_PROPERTY.equalsIgnoreCase(valueToFilterBy);
+	//				System.out.println("DEBUG:\t\t[ActionProperty.ruleIncludesAction] no value for action=\n" + action + "\nRule=" + this);
 				}
 			}
 			else {
-				return MonitorConstants.BLANK_PROPERTY.equalsIgnoreCase(valueToFilterBy);
-//				System.out.println("DEBUG:\t\t[ActionProperty.ruleIncludesAction] no value for action=\n" + action + "\nRule=" + this);
+				System.err.println("ERROR:\t\t[ActionProperty.ruleIncludesAction] valueToFilterBy is null for Rule=" + this);
 			}
 		}
-		else {
-			System.err.println("ERROR:\t\t[ActionProperty.ruleIncludesAction] valueToFilterBy is null for Rule=" + this);
+		catch(Exception e){
+			System.err.println("ERROR\t\t[ActionPropertyRule.ruleIncludesAction] action \n"+ action + "\n rule: " + this +" \n" + e);
 		}
 		return false;
 	}
