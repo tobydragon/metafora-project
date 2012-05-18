b8133661bedd3cb48b9cb8357d58104e10847509
ugurkira <ugurkira@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2012-05-18 22:19:06 +0000

    Tagging is activated for all actions

--- trunk/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/monitor/AnalysisMonitorListener.java
+++ trunk/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/monitor/AnalysisMonitorListener.java
@@ -1,19 +1,22 @@
 package de.uds.MonitorInterventionMetafora.server.monitor;
 
+import de.uds.MonitorInterventionMetafora.server.analysis.manager.TaggingManager;
 import de.uds.MonitorInterventionMetafora.server.cfcommunication.CfCommunicationListener;
 import de.uds.MonitorInterventionMetafora.shared.commonformat.CfAction;
 
 public class AnalysisMonitorListener implements CfCommunicationListener{
 
 	private MonitorModel model;
+	private TaggingManager taggingManger;
 	
 	public AnalysisMonitorListener(MonitorModel monitorModel){
 		this.model = monitorModel;
+		taggingManger=new TaggingManager();
 	}
 	
 	//synchronized because it can be registered to more than one manager, and so calls should be synchronized
 	public synchronized void processCfAction(String user, CfAction action) {
-		model.addAction(action);
+		model.addAction(taggingManger.tagAction(action));
 	}
 
 }
