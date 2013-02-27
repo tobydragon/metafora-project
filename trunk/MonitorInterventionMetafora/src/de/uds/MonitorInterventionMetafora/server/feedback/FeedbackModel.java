0b17639a2733c7e2c4d34157e90db73d75e3e5ac
tdragon1999@gmail.com <tdragon1999@gmail.com@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2013-02-27 12:22:13 +0000

    Added FeedbackController and FeedbackModel for framework on server side.

---
+++ trunk/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/feedback/FeedbackModel.java
@@ -0,0 +1,9 @@
+package de.uds.MonitorInterventionMetafora.server.feedback;
+
+public class FeedbackModel {
+	
+	//SugestedMessagesModel
+	
+	//updateSuggestedMessages(String username, String messagesXml){}
+
+}
