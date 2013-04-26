2ea9265c19d82d20188a19f0a4e43f69592cd17f
toontalk@gmail.com <toontalk@gmail.com@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2013-04-26 16:18:00 +0000

    Committed sample files for localisation of messages/strings. To use one needs something like
    public static MessagesBundle messagesBundle = (MessagesBundle) GWT.create(MessagesBundle.class);
    
    and then to access the localised String with 
    
    messagesBundle.Repeat()
    
    The MonitorInterventionMetafora.gwt.xml file needs to be extended to include
    
      <inherits name="com.google.gwt.i18n.I18N" />
     
      <extend-property name="locale" values="en" />
      <extend-property name="locale" values="es_ES" />

---
+++ trunk/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/client/messages/MessagesBundle.java
@@ -0,0 +1,16 @@
+
+package de.uds.MonitorInterventionMetafora.client.messages;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * GWT client interface to the messages bundle
+ * 
+ * property files can be defined in different languages
+ * 
+ * @author Ken Kahn
+ *
+ */
+public interface MessagesBundle extends Constants {
+    String Repeat();
+}
