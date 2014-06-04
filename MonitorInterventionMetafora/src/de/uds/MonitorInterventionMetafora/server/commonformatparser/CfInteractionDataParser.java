7717d41f04bb921424c66bc8c7ef8d808f483372
cawormsley <cawormsley@gmail.com>
2014-06-04 14:10:36 +0000

    Update Xml work

--- MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/commonformatparser/CfInteractionDataParser.java
+++ MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/server/commonformatparser/CfInteractionDataParser.java
@@ -9,7 +9,11 @@ import de.uds.MonitorInterventionMetafora.server.xml.XmlFragment;
 import de.uds.MonitorInterventionMetafora.shared.commonformat.CfAction;
 import de.uds.MonitorInterventionMetafora.shared.commonformat.CfInteractionData;
 import de.uds.MonitorInterventionMetafora.shared.commonformat.CommonFormatStrings;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import de.uds.MonitorInterventionMetafora.shared.commonformat.RunestoneStrings;
+
 
 
 
@@ -61,8 +65,25 @@ public class CfInteractionDataParser {
 		XmlFragment runestoneFrag = XmlFragment.getFragmentFromLocalFile("war/conffiles/xml/test/runestoneXml/runestoneExample.xml");
 		logger.info(runestoneFrag.toString());
 		
-		logger.info(runestoneFrag.getChildren("row").get(0).getChildValue("act"));
+		String ts = (runestoneFrag.getChildren("row").get(0).getChildValue("timestamp"));
+	
 		
+		//converts a time stamp to epoch
+		DateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //not sure if to use HH (0-23) or kk (1-24)
+		try{
+		long date1 = dateForm.parse(ts).getTime();
+		long date2 = dateForm.parse("2014-01-29 19:22:05").getTime();
+		long date3 = dateForm.parse("2014-06-13 3:42:32").getTime();
+		
+		
+		System.out.println("Epoch time of 2014-01-29 19:22:12: " + (date1/1000)); //divide by 1000 to convert to seconds
+		System.out.println("Epoch time of 2014-01-29 19:22:05: " + (date2/1000));
+		System.out.println("Epoch time of 2014-06-13 3:42:32: " + (date3/1000));
+		}
+		catch (ParseException e){
+			;
+		
+		}
 	
 	}
 
