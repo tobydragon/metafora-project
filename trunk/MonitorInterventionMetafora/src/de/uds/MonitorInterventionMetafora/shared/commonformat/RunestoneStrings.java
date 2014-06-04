e185fd08b4d6cb6b3f79e639c2ec9c1b8870abff
dr.toby.dragon@gmail.com <dr.toby.dragon@gmail.com@d4a8ba5b-d184-7677-b597-0b9f40b331d2>
2014-06-04 14:07:27 +0000

    Continued work on runsetone xml input

---
+++ trunk/MonitorInterventionMetafora/src/de/uds/MonitorInterventionMetafora/shared/commonformat/RunestoneStrings.java
@@ -0,0 +1,10 @@
+package de.uds.MonitorInterventionMetafora.shared.commonformat;
+
+public class RunestoneStrings {
+
+	public static final String ROWS_STRING = "rows";
+	public static final String ROW_STRING = "row";
+	
+	
+
+}
